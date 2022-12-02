14c09ff1dee0eeefe7120a6e37293e1e3a633dd0
Shane <shaneconway1998@gmail.com>
2022-12-02 00:11:15 +0000

    added check_word function to compare the users word and see if it matches the random word

--- run.py
+++ run.py
@@ -2,9 +2,26 @@ import random
 
 
 def get_word():
+    """
+    Gets a random 5 letter word from the list of words
+    """
     file = open("assets/words.txt", "r")
     word = random.choice(file.readlines()).strip("\n")
     print(word)
 
 
-get_word()
+def check_word(str):
+    """
+    Contains logic for checking if the users word
+    is equal to the random word, logic needs to be added
+    """
+    for char in str:
+        if char in str:
+            print("Good job!")
+
+
+word = get_word()
+
+print("Please enter a word: ")
+user_word = input()
+check_word(user_word)
