c217c0d74267a90f2b5b0499095bc04ba10bd3b4
Shane <shaneconway1998@gmail.com>
2022-12-02 01:01:22 +0000

    added the check_word logic

--- run.py
+++ run.py
@@ -10,16 +10,39 @@ def get_word():
     file = open("assets/words.txt", "r")
     random_word = random.choice(file.readlines()).strip("\n")
     print(random_word)
+    return random_word
 
 
-def check_word(word):
+def check_word(guess_word):
     """
     Contains logic for checking if the users word
     is equal to the random word, logic needs to be added
     """
-    for char in word:
-        if char in word:
-            print("Good job!")
+    random_word = get_word()
+
+    if guess_word == random_word:
+        print("Congratulations you win!")
+    else:
+        for c, w in zip(random_word, guess_word):
+            if w in random_word and w in c:
+                print(w + " Y ")
+            elif w in random_word:
+                print(w + " D ")
+            else:
+                print(" N ")
+
+
+def instructions():
+    """
+    Instructions for the user on how to play the game
+    """
+    print("""Wordle is a single player game
+A player has to guess a five letter hidden word
+You have six attempts
+Your Progress Guide "YNNYD"
+"Y" Indicates that the letter at that position was guessed correctly
+"D" indicates that the letter at that position is in the hidden word, but in a different position
+"N" indicates that the letter at that position is wrong, and isn't in the hidden word   """)
 
 
 word = get_word()
