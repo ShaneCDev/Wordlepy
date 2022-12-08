4d5cd342d3bdb8caf872ee601030635acbbc9098
Shane <shaneconway1998@gmail.com>
2022-12-08 16:56:24 +0000

    added new run game and new check word function, loop is currently broken need to fix

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 import random
 import re
-import curses
+from colorama import init, Fore, Back, Style
 
-
-
+init()
 def get_word():
     """
     Gets a random 5 letter word from the list of words
@@ -14,11 +13,11 @@ def get_word():
     file.close()
     return random_word
 
-
+"""
 def check_word(guess_word):
     """
-    Contains logic for checking if the users word
-    is equal to the random word, logic needs to be added
+    #Contains logic for checking if the users word
+    #is equal to the random word, logic needs to be added
     """
     random_word = get_word()
 
@@ -27,11 +26,12 @@ def check_word(guess_word):
     else:
         for c, w in zip(random_word, guess_word):
             if w in random_word and w in c:
-                print(w + curses.COLOR_GREEN)
+                print(w + str(curses.COLOR_GREEN))
             elif w in random_word:
-                print(w + curses.COLOR_YELLOW)
+                print(w + str(curses.COLOR_YELLOW))
             else:
-                print(curses.COLOR_RED)
+                print(str(curses.COLOR_RED))
+"""
 
 
 def check_for_special_char(string):
@@ -43,9 +43,9 @@ def check_for_special_char(string):
     special_char_check = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
 
     if special_char_check.search(string) is None:
-        print("Special characters are not present")
+        return True
     else:
-        print("String contains special characters")
+        return False
 
 
 def instructions():
@@ -67,11 +67,11 @@ Your Progress Guide "YNNYD"
 "D" indicates that the letter at that position is in the hidden word, but in a different position
 "N" indicates that the letter at that position is wrong, and isn't in the hidden word   """)
 
-
+"""
 def run_game():
     """
-    Function that puts all other functions together
-    forming the game.
+    #Function that puts all other functions together
+    #forming the game.
     """
     attempts = 6
     instructions()
@@ -91,6 +91,38 @@ def run_game():
                     print("You entered a special character, please try again!")
     except ValueError:
         print("You entered a special character, please try again!")
+"""
 
 
-instructions()
+def new_run_game():
+    attempts = 6
+    instructions()
+    print("Press [Y] to play or [Q] to quit. \n")
+    user_choice = input()
+    if check_for_special_char(user_choice) is True:
+        if user_choice.upper() == "Y":
+            print("Enter your guess: \n")
+            user_guess = input()
+            while attempts > 0:
+                game_logic(user_guess, attempts)
+                attempts = attempts - 1
+
+
+def game_logic(user_word, attempts):
+    random_word = get_word()
+    print("The word is: " + random_word)
+    output = ""
+    while attempts > 0:
+        for i, char in enumerate(random_word):
+            if user_word[i] == random_word[i]:
+                output = output + Back.RED + char + Back.RESET
+            elif user_word[i] in random_word:
+                output = output + Back.YELLOW + char + Back.RESET
+            else:
+                output = output + char + Back.RESET
+        print(output)
+        if user_word == random_word:
+            print(f"Congratulations you guessed the word in {attempts} guesses.")
+        attempts = attempts - 1
+
+new_run_game()
